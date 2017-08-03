7d235c7139e1afc0011dacc547f168ca3e04bfa1
Greg Dardis <dardis.greg@gmail.com>
2017-08-03 08:13:44 -0600

    implemented bag class, removed bin from github

--- src/Bag.java
+++ src/Bag.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Bag<Item> implements Iterable<Item> {
 
@@ -16,9 +17,26 @@ public class Bag<Item> implements Iterable<Item> {
         first = null;
     }
     
-    // size()
-    // isEmpty()
-    // add()
+    public int size() {
+        return bagSize;
+    }
+    
+    public boolean isEmpty() {
+        return first == null;
+    }
+    
+    public void add(Item item) {
+        if (isEmpty()) {
+            first = new Node();
+            first.item = item;
+        } else {
+            Node oldFirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = oldFirst;
+        }
+        bagSize++;
+    }
     
     public Iterator<Item> iterator() {
         return new BagIterator();
@@ -29,14 +47,24 @@ public class Bag<Item> implements Iterable<Item> {
         private Node current = first;
         
         public Item next() {
-            
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public boolean hasNext() {
+            return current != null;
         }
         
         public void remove() {
             throw new UnsupportedOperationException();
         }
-        // next
-        // hasNext
-        // remove throws exception
+    }
+    
+    public static void main(String[] args) {
+        // testing
     }
 }
